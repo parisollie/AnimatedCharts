7f61a497913213265feb0b23e36d08014605603e
parisollie <pjff18@gmail.com>
2025-03-27 14:14:31 -0600

    Initial Commit

---
+++ AnimatedCharts/AnimatedChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedChartsApp.swift
+//  AnimatedCharts
+//
+//  Created by Paul F on 27/03/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
